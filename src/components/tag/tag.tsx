7a49c7211053b22a595811e18b832d99594ebb56
Tim Best <timothy.best@hq.dhs.gov>
2020-03-05 13:28:37 -0500

    todo

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -2,6 +2,7 @@ import { Component, h } from "@stencil/core";
 
 const component = "usa-tag";
 
+// TODO add tag big?
 @Component({
   tag: "usa-tag",
   styleUrl: "../../../uswds/src/stylesheets/packages/_usa-tag.scss"
